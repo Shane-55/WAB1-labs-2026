99b2cc91b347d7112bc10ea026685a0ea53b439c
(20116119) Shane Harkin <20116119@setu.ie>
2026-02-26 15:41:02 +0000

    Add Models and Views

--- models/employeeLink.js
+++ models/employeeLink.js
@@ -1,6 +1,18 @@
-import appInfo from "./app-store"
-import employee from "./employee1.json"
+'use strict';
 
-getAppInfo() {
+import logger from '../utils/logger.js';
+import JsonStore from './json-store.js';
+
+const employeeLink = {
+
+  store: new JsonStore('./models/employee1.json', { employee: {} }),
+  collection: 'employee',
+
+
+  getEmpInfo() {
     return this.store.findAll(this.collection);
   },
+
+};
+
+export default employeeLink;
